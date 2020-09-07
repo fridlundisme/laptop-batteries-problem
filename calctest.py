53084257eb53952009fd086bb00d96c3a4f60b47
fridlund <friddelund@gmail.com>
2020-09-07 20:42:24 +0200

    Done

--- calctest.py
+++ calctest.py
@@ -1,26 +1,18 @@
-import numpy as np
+import fileinput
 import math
 
-ten = np.array([1,2,3,4,5,6,7,8,9])
-twtre = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22])
 
 
-def splitThird(array, n):
-    if(len(array) <= 4):
-        print("End recursion " + str(len(array) - 1))
-        return len(array) - 1
-    else:
-        oneThird = math.floor(len(array)/3)
-        array1 = array[0:oneThird]
-        array2 = array[oneThird+1:None]
+if __name__ == "__main__":
+    testCases = []
+    for lines in fileinput.input():
+        testCases.append(int(lines.rstrip()))
 
-        print(array1)
-        print(array2)
-        n += 1
-        print("iterations " + str(n))
-        return n + splitThird(array2, n)
-
-
-
-en = splitThird(ten, 0)
-print(en)
+    for case in testCases:
+        if case == 0:
+            break
+        # the nth number is already known to give explosion
+        case = case - 1
+        # by triangle equation
+        n = math.ceil((math.sqrt(8*case + 1) - 1) / 2)
+        print(n)
